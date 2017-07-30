30bd2e49c413fdbb4cc2de652a793d78e697618d
Jordon R <reese.jordon@gmail.com>
2017-07-30 12:38:40 -0500

    RockPaperScissors refactor

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -10,33 +10,13 @@ const rl = readline.createInterface({
 
 function rockPaperScissors(hand1, hand2) {
 
-  if (hand1 === hand2){
-    return "It's a tie!";
-  }else if (hand1 === 'rock' && hand2 === 'paper'){
-    return "Hand two wins!";
-  }else if (hand1 === 'rock' && hand2 === 'scissors'){
-    return "Hand one wins!";
-  }else if (hand1 === 'scissors' && hand2 === 'rock'){
-    return "Hand two wins!";
-  }else if (hand1 === 'scissors' && hand2 == 'paper'){
-    return "Hand one wins!";
-  }else if (hand1 === 'paper' && hand2 === 'rock'){
-    return "Hand 1 wins!";
-  }else {
-    return "Hand two wins!";
-  }
+  if (hand1 === hand2) {
+    return `It's a tie!`;
+  };
+  return hand2 === 'rock' ? 'rock loses' : 'paper wins!';
+  return hand2 === 'paper' ? 'paper loses' : 'paper wins!';
+  return hand2 === 'scissors' ? 'scissors loses' : 'scissors wins!';
 }
-
-// }else if (hand1 === 'rock' && hand2 === 'rock'){
-//   return "It's a tie!";
-// }else if (hand1 === 'scissors' && hand2 === 'scissors'){
-//   return "It's a tie!";
-// }else if (hand1 === 'paper' && hand2 === 'paper'){
-//   return "It's a tie!";
-
-
-
-
 
 function getPrompt() {
   rl.question('hand1: ', (answer1) => {
