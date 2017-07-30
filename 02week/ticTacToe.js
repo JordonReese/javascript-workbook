ea199e364868690046370c9f59b7ce937d6916fb
Jordon R <reese.jordon@gmail.com>
2017-07-29 21:00:27 -0500

    Submission for TicTacToe

--- 02week/ticTacToe.js
+++ 02week/ticTacToe.js
@@ -24,23 +24,65 @@ function printBoard() {
 }
 
 function horizontalWin() {
-  // Your code here
+  // if all column 0 equals variable playerTurn
+  // if all column 1 equals variable playerTurn
+  // if all column 2 equals variable playerTurn
+  // current player wins!
+  if (
+    (board[0][0] === playerTurn && board[0][1] === playerTurn && board[0][2] === playerTurn) ||
+    (board[1][0] === playerTurn && board[1][1] === playerTurn && board[1][2] === playerTurn) ||
+    (board[2][0] === playerTurn && board[2][1] === playerTurn && board[2][2] === playerTurn)) {
+    return true;
+  }
 }
 
 function verticalWin() {
-  // Your code here
+  // if board equals column 0 vertical win
+  // if board equals column 1 vertical win
+  // if board equals column 2 vertical win
+  // Player '' wins!
+  if ((board[0][0] === playerTurn && board[1][0] === playerTurn && board[2][0] === playerTurn) ||
+  (board[0][1] === playerTurn && board[1][1] === playerTurn && board[2][1] === playerTurn) ||
+  (board[0][2] === playerTurn && board[1][2] === playerTurn && board[2][2] === playerTurn)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function diagonalWin() {
-  // Your code here
+  // if board for current player left diagnal is same
+  // if board for current player right diagnal is the same
+  // current player wins
+  return ((board[0][0] === playerTurn && board[1][1] === playerTurn && board[2][2] === playerTurn) ||
+  (board[0][2] === playerTurn && board[1][1] === playerTurn && board[2][0] === playerTurn));
 }
 
+
 function checkForWin() {
-  // Your code here
+  // does current player have a horizontal win
+  // does current player have a vertical win
+  // does current player have a diagnal win
+  if (horizontalWin() || verticalWin() || diagonalWin()) {
+    console.log('Player ' + playerTurn + ' Won!');
+    return true;
+  } else {
+    return false;
+  }
+}
+
+var isSpaceAvailable = function(row, column) {
+  return ( board[row][column] === ' ' );
 }
 
 function ticTacToe(row, column) {
-  // Your code here
+  if (isSpaceAvailable(row, column)) {
+    board[row][column] = playerTurn;
+    checkForWin();
+    playerTurn = (playerTurn === 'X') ? 'O' : 'X';
+  } else {
+    console.log('This space is already taken. Try another space.');
+  }
 }
 
 function getPrompt() {
